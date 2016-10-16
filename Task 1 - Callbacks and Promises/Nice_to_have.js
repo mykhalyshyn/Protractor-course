8110657536c3fee6e9eee37040b0c2f727b11cef
mykhalyshyn <Meh@Serhiis-MBP.Dlink>
2016-10-17 00:59:17 +0300

    Initial commit

---
+++ Task 1 - Callbacks and Promises/Nice_to_have.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Serhii Mykhalyshyn on 10/16/16.
+ */
+// 2) Describe in Promises: (3 points)
+// You get all items from API
+// Save id’s of this items
+// Delete all items with DELETE request with ‘for’ cycle.
+//     Check with promise all that all server answers has correct response code.
+//     Check that server has no items with ‘assert’ library.
---
+++ Task 1 - Callbacks and Promises/Promises.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Serhii Mykhalyshyn on 10/16/16.
+ */
+// PROMISES:
+//     1) Create POST request which will be in promise. Describe onSuccess(201 response) and onFailure(422 response) variants.(1 points)
+// 2) Create POST request which will be rejected always, even you set correct parameters. (1 point)
+// 3) Create 3 POST requests. Check that all responses codes equal 200. (1 point)
+// 4) Create POST request with invalid parameters. But you should catch error, and return 200 response code.(1 point)
